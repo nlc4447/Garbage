004e7c78a68fb57fb671512f6431d9e67bf2fce7
nlc4447 <54862227+nlc4447@users.noreply.github.com>
2021-04-30 20:40:33 -0400

    Hand constructor implemented

--- Hand.java
+++ Hand.java
@@ -1,14 +1,28 @@
 /*
 Representation of the current hand, includes a two dimensional array of the cards as the hand
 */
-import java.util.ArrayList;
+
 
 public class Hand{
     
     private Card[][] hand;
+    private Deck deck;
 
     public Hand(Deck deck){
-        this.hand = hand;
+        this.deck = deck;
+        for(int i = 0; i < 5; i++){
+            for(int j = 0; j < 5; j++){
+                hand[i][j] = deck.getTopCard();
+            }
+        }
+    }
+
+    public Deck getUpdatedDeck(){
+        return deck;
+    }
+
+    public Card[][] getHand(){
+        return hand;
     }
 
     
